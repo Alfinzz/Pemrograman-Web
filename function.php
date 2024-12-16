660fc8c7432c71f4dd7b6136df30c7c0dd51d5e3
Alfian57 <alfiangadingsaputra@gmail.com>
2024-12-16 15:48:31 +0700

    fix: update env file

--- function.php
+++ function.php
@@ -2,11 +2,17 @@
 session_start();
 if (file_exists('.env')) {
     $env = parse_ini_file('.env');
+    $dbHost = $env["DB_HOST"];
+    $dbUsername = $env["DB_USERNAME"];
+    $dbPassword = $env["DB_PASSWORD"];
+    $dbName = $env["DB_NAME"];
+} else {
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    $dbName = getenv("DB_NAME");
 }
-$dbHost = $env["DB_HOST"];
-$dbUsername = $env["DB_USERNAME"];
-$dbPassword = $env["DB_PASSWORD"];
-$dbName = $env["DB_NAME"];
+
 
 $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
